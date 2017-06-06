d9270924f62a6e619abd4bff3a0c3fd7840c678c
Marcin Chrzanowski <marcin.j.chrzanowski@gmail.com>
2017-06-07 00:35:25 +0200

    Change bar color with time

--- src/components/TimeBarView.js
+++ src/components/TimeBarView.js
@@ -5,9 +5,15 @@ import styles from '../styles/styles'
 export const TimeBarView = ({timeLeft}) => {
   let { width } = Dimensions.get('window')
   let barWidth = Math.round((timeLeft / 600) * width)
+  let color = 'green'
+  if (timeLeft <= 150) {
+    color = 'red'
+  } else if (timeLeft <= 300) {
+    color = 'orange'
+  }
 
   return (
-    <View style={ [styles.timeBar, { width: barWidth }] }></View>
+    <View style={ [styles.timeBar, { width: barWidth, backgroundColor: color }] }></View>
   )
 }
 
